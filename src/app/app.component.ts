5c1bea7a19aa1b7aeda6de381581265696233de9
Ilyeschrif22 <ilyes.chrif@esprit.tn>
2025-06-09 03:03:32 +0200

    vercel deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
-  title = 'Portfolio';
+  constructor() {}
 }
